9244b7f2c6f612038ca4660970ccfe1165335d03
Linh Quach <Linh.M.Quach1@gmail.com>
2021-04-01 17:09:19 -0500

    Temporary Table Exercises

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -1,4 +1,5 @@
 USE florence23;
+
 -- 1. Using the example from the lesson, re-create the employees_with_departments table.
 CREATE TEMPORARY TABLE employees_with_departments AS
 SELECT emp_no, first_name, last_name, dept_no, dept_name
@@ -17,6 +18,7 @@ ALTER TABLE employees_with_departments ADD full_name VARCHAR(30);
 UPDATE employees_with_departments
 SET full_name = CONCAT(first_name, ' ', last_name);
 
+
 -- C. Remove the first_name and last_name columns from the table.
 ALTER TABLE employees_with_departments DROP COLUMN first_name;
 ALTER TABLE employees_with_departments DROP COLUMN last_name;
@@ -29,14 +31,18 @@ FROM employees.employees
 JOIN employees.dept_emp USING(emp_no)
 JOIN employees.departments USING(dept_no);
 
--- 2. Create a temporary table based on the payment table from the sakila database.
+
+
+-- 2. Create a temporary table based on the payment table 
+-- from the sakila database.
 CREATE TEMPORARY TABLE Kila AS 
 SELECT *
 FROM sakila.payment;
 
 
--- A. Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
-
+-- A. Write the SQL necessary to transform the amount column such 
+-- that it is stored as an integer representing the 
+-- number of cents of the payment. For example, 1.99 should become 199.
 
 ALTER TABLE Kila
 MODIFY COLUMN amount DECIMAL(7,2);
@@ -51,7 +57,10 @@ MODIFY COLUMN amount INT;
 
 
 
--- 3. Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?
+-- 3. Find out how the current average pay in each department 
+-- compares to the overall, historical average pay. 
+-- In order to make the comparison easier, you should use the Z-score for salaries.
+-- In terms of salary, what is the best department right now to work for? The worst?
 
 select avg(salary) as avg_salary, std(salary) as std_salary
 from employees.salaries;
